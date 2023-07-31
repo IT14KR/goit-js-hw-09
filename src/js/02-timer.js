6fc1f2eabe7bef53f6efeeb20887ba67f8a05adb
IT14KR <131267129+IT14KR@users.noreply.github.com>
2023-07-31 23:30:53 +0200

    last commit

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,53 +1,86 @@
-import flatpickr from 'flatpickr';
-import 'flatpickr/dist/flatpickr.min.css';
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
 
-const refs = {
-  startBtn: document.querySelector('.js-btn-start'),
-  days: document.querySelector('.js-days'),
-  hours: document.querySelector('.js-hours'),
-  minutes: document.querySelector('.js-minutes'),
-  seconds: document.querySelector('.js-seconds'),
-  'datetime-picker': document.querySelector('#datetime-picker'),
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    const selectedDate = selectedDates[0];
+    const currentDate = new Date();
+
+    if (selectedDate <= currentDate) {
+      window.alert("Please choose a date in the future");
+      refs.dateTimePicker.value = ""; 
+      return;
+    }
+
+    refs.startButton.disabled = false;
+  },
 };
 
-function convertMs(ms) {
-  const second = 1000;
-  const minute = second * 60;
-  const hour = minute * 60;
-  const day = hour * 24;
 
-  const days = Math.floor(ms / day);
-  const hours = Math.floor((ms % day) / hour);
-  const minutes = Math.floor(((ms % day) % hour) / minute);
-  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+const refs = {
+  startButton: document.querySelector(".js-btn-start"),
+  days: document.querySelector(".js-days"),
+  hours: document.querySelector(".js-hours"),
+  minutes: document.querySelector(".js-minutes"),
+  seconds: document.querySelector(".js-seconds"),
+  dateTimePicker: document.getElementById("datetime-picker"),
+};
 
-  return { days, hours, minutes, seconds };
-}
+flatpickr(refs.dateTimePicker, options);
 
-function addLeadingZero(value) {
-  return String(value).padStart(2, '0');
-}
-
-refs.startBtn.addEventListener('click', () => {
-  const chosenDate = new Date(flatpickr.parseDate(refs.datetimePicker.value));
-
+refs.startButton.addEventListener("click", () => {
+  const selectedDate = flatpickr.parseDate(refs.dateTimePicker.value);
   const currentDate = new Date();
-  let timeLeft = chosenDate - currentDate;
 
-  let intervalId = setInterval(() => {
-    const { days, hours, minutes, seconds } = convertMs(timeLeft);
-    refs.days.textContent = addLeadingZero(days);
-    refs.hours.textContent = addLeadingZero(hours);
-    refs.minutes.textContent = addLeadingZero(minutes);
-    refs.seconds.textContent = addLeadingZero(seconds);
-    timeLeft -= 1000;
+  if (selectedDate <= currentDate) {
+    window.alert("Please choose a date in the future");
+    return;
+  }
 
-    if (timeLeft < 0) {
-      clearInterval(intervalId);
-      refs.days.textContent = '00';
-      refs.hours.textContent = '00';
-      refs.minutes.textContent = '00';
-      refs.seconds.textContent = '00';
+  refs.startButton.disabled = true;
+  let difference = selectedDate.getTime() - currentDate.getTime();
+  let countdownInterval;
+
+  function convertMs(ms) {
+    function addLeadingZero(value) {
+      return value.toString().padStart(2, "0");
     }
+
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+
+    const days = Math.floor(ms / day);
+    const hours = Math.floor((ms % day) / hour);
+    const minutes = Math.floor(((ms % day) % hour) / minute);
+    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+    return {
+      days: addLeadingZero(days),
+      hours: addLeadingZero(hours),
+      minutes: addLeadingZero(minutes),
+      seconds: addLeadingZero(seconds),
+    };
+  }
+
+  countdownInterval = setInterval(() => {
+    const remainingTime = convertMs(difference);
+
+    refs.days.textContent = remainingTime.days;
+    refs.hours.textContent = remainingTime.hours;
+    refs.minutes.textContent = remainingTime.minutes;
+    refs.seconds.textContent = remainingTime.seconds;
+
+    if (difference <= 0) {
+      clearInterval(countdownInterval);
+      console.log("Countdown has finished!");
+    }
+
+    difference -= 1000;
   }, 1000);
 });
